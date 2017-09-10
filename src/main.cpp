fa5d53c38c1985803a50b549f3de1e2506ce53ba
neeh <nicolas.g@gmx.com>
2017-09-10 21:50:55 +0100

    Add some palindrome algorithms

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+#include "palindrome.hpp"
+
+int main() {
+  std::string s = "OiTeSeTiO";
+  std::cout << s << " " << (isPalindromePtr(s) ? "is" : "is not") << " a palindrome" << std::endl;
+
+  bool res;
+
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  start = std::chrono::system_clock::now();
+  for (int i = 0; i < 10000000; i++) {
+    res = isPalindromePtr(s);
+  }
+  std::cout << res << std::endl;
+  end = std::chrono::system_clock::now();
+
+  std::chrono::duration<double> elapsed_s = end - start;
+
+  std::cout << elapsed_s.count() << std::endl;
+
+
+  std::string longStr = "aizajSdozdjaoAiiiidAsajkdjqUjzjjjjjUzkkksdqqq";
+  std::cout << longestPalindromeMap(longStr) << std::endl;
+
+  return 0;
+}
---
+++ src/palindrome.cpp
@@ -0,0 +1,78 @@
+#include "palindrome.hpp"
+
+#include <unordered_map>
+
+
+// isPalindrome
+
+bool isPalindrome(std::string str) {
+  // 100%
+  uint32_t l = 0;
+  uint32_t r = str.size() - 1;
+  while (r > l) {
+    if (str[l++] != str[r--]) return false;
+  }
+  return true;
+}
+
+bool isPalindromePtr(std::string str) {
+  // 67%
+  const char *l = str.c_str();
+  const char *r = l + str.size() - 1;
+  while (r > l) {
+    if (*l++ != *r--) return false;
+  }
+  return true;
+}
+
+bool isPalindromeIterator(std::string str) {
+  // 1077%
+  return str == std::string(str.rbegin(), str.rend());
+}
+
+bool isPalindromeEqual(std::string str) {
+  // 363%
+  return std::equal(str.begin(), str.begin() + str.size() / 2, str.rbegin());
+}
+
+
+// longestPalindrome
+
+std::string longestPalindrome(std::string str) {
+  if (isPalindrome(str)) {
+    return str;
+  } else {
+    return str;
+  }
+}
+
+std::string longestPalindromeMap(std::string str) {
+  if (isPalindrome(str)) return str;
+
+  std::unordered_map<char, uint32_t> cache;
+  const char *p = str.c_str();
+  const char *end = p + str.size();
+
+  while (p < end) {
+    if (cache.find(*p) != cache.end()) {
+      cache[*p]++;
+    } else {
+      cache.insert(std::make_pair(*p, 0));
+    }
+    p++;
+  }
+
+  std::string out = "";
+
+  for (std::pair<char, uint32_t> kv : cache) {
+    char c = kv.first;
+    uint32_t count = kv.second;
+
+    while (count > 1) {
+      out = c + out + c;
+      count -= 2;
+    }
+  }
+
+  return out;
+}
